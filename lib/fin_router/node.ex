3cd9bd09a5e9a1b8edcadf9bfb9a8930673ad56d
codehans <94654388+codehans@users.noreply.github.com>
2024-04-08 16:11:58 +0100

    upgrade kujira-ex

--- lib/fin_router/node.ex
+++ lib/fin_router/node.ex
@@ -1,28 +1,6 @@
 defmodule FinRouter.Node do
-  use Supervisor
-  require Logger
-
-  def start_link(opts) do
-    Supervisor.start_link(__MODULE__, :ok, opts)
-  end
-
-  @impl true
-  def init(:ok) do
-    config = Application.get_env(:fin_router, __MODULE__)
-
-    children = [
-      {__MODULE__.Grpc, config},
-      {__MODULE__.Websocket, config}
-    ]
-
-    Supervisor.init(children, strategy: :one_for_one, name: __MODULE__.Supervisor)
-  end
-
-  def channel() do
-    __MODULE__.Grpc.channel()
-  end
-
-  def subscribe(topic) do
-    Phoenix.PubSub.subscribe(FinRouter.PubSub, topic)
-  end
+  use Kujira.Node,
+    otp_app: :fin_router,
+    pubsub: FinRouter.PubSub,
+    subscriptions: ["tm.event='NewBlock'"]
 end
--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule FinRouter.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:kujira, "~> 0.1.19"},
+      {:kujira, "~> 0.1.21"},
       {:websockex, "~> 0.4.3"}
     ]
   end
